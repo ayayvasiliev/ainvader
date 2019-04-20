3cf7fdbdc1f24c7be83d9f35a2e7fd9b12245f7f
NyanCoder <VityaNiko@Mail.ru>
2019-04-20 23:20:53 +0900

    Организовал дирокторию ассетов

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	private float[] inputs;
+	private float[] weights;
+	private float[] outputs;
+
+
+	public float this[int i] {
+		set { inputs[i % inputs.Length] = value; }
+		get { return outputs[i % outputs.Length]; }
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		inputs = new float[5];
+		outputs = new float[3];
+		weights = new float[inputs.Lenght * outputs.Lenght];
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+	void SetWeight(int inputIndex, int outputIndex, float value) {
+		weights[inputIndex % inputs.Lenght + (outputIndex % outputs.Lenght) * inputs.Lenght] = value;
+	}
+	float GetWeight(int inputIndex, int outputIndex) {
+		return weights[inputIndex % inputs.Lenght + (outputIndex % outputs.Lenght) * inputs.Lenght];
+	}
+
+	private static float Sigmoid(double value) {
+		float k = Math.Exp(value);
+		return k / (1.0f + k);
+	}
+
+	private void Calc() {
+		for (int i = 0; i < outputs.Lenght; i++)
+		{
+			double value = 0.0;
+			for (int j = 0; j < inputs.Lenght; j++)
+				value = this[j] * GetWeight(j, i);
+			this[i] = Sigmoid(value);
+		}
+	}
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject enemy;                // The enemy prefab to be spawned.
+    public float spawnTime = 3f;            // How long between each spawn.
+    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("spawn", spawnTime, spawnTime);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+
+	private void spawn() {
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate (enemy, this., spawnPoints[spawnPointIndex].rotation);
+	}
+}
